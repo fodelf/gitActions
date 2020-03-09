8c4dc7b14971c7e4bb68f5560f5434824432f10d
wuwenzhou <wuwenzhou@yunzhangfang.com>
2020-03-09 20:26:29 +0800

    c测试

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,10 @@
  * @Github: http://gitlab.yzf.net/wuwenzhou
  * @Date: 2020-02-28 15:23:01
  * @LastEditors: 吴文周
- * @LastEditTime: 2020-02-28 15:23:40
+ * @LastEditTime: 2020-03-09 20:24:44
  */
 module.exports = {
+  lintOnSave: false,
   outputDir: 'dist',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/gitActions/'
