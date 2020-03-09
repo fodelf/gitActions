2b5ce920ac37f29200b23f0fcd222e551c3aec82
吴文周 <wuwenzhou@qq.com.cn>
2020-03-09 23:09:47 +0800

    测试

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,12 @@
  * @Github: http://gitlab.yzf.net/wuwenzhou
  * @Date: 2020-02-28 15:23:01
  * @LastEditors: 吴文周
- * @LastEditTime: 2020-03-09 22:26:15
+ * @LastEditTime: 2020-03-09 23:09:36
  */
 const ispro = process.env.NODE_ENV !== 'development'
 module.exports = {
   lintOnSave: false,
-  outputDir: 'dist',
+  // outputDir: 'dist',
   publicPath: ispro ? '' : '/',
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/gitActions/'
